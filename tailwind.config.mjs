14c9233a05b783d363706c77f231411cabe8e065
arismercedes <pyroaq@hotmail.com>
2024-04-30 22:29:00 -0500

    added button component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'crimson': {
+					50: '#fff1f2',
+					100: '#ffe0e2',
+					200: '#ffc7cb',
+					300: '#ff9fa7',
+					400: '#ff6874',
+					500: '#fa3948',
+					600: '#e31727',
+					700: '#c31220',
+					800: '#a1131e',
+					900: '#851720',
+					950: '#49060b',
+				},
+			},
+		}
 	},
 	plugins: [],
 }
